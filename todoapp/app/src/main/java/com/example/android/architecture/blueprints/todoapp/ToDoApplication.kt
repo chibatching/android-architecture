6ae054d29498888791129a554317727d838f82e6
Takao Chiba <takao.chiba@gmail.com>
2016-07-21 22:44:58 +0900

    Use Kodein.lazy instead of LazyKodein

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/ToDoApplication.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/ToDoApplication.kt
@@ -5,16 +5,14 @@ import android.content.Context
 import com.example.android.architecture.blueprints.todoapp.data.source.tasksRepositoryModule
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.KodeinAware
-import com.github.salomonbrys.kodein.LazyKodein
+import com.github.salomonbrys.kodein.lazy
 import com.github.salomonbrys.kodein.provider
 
 
 class ToDoApplication : Application(), KodeinAware {
 
-    override val kodein: Kodein by LazyKodein {
-        Kodein {
-            import(tasksRepositoryModule)
-            bind<Context>() with provider { this@ToDoApplication }
-        }
+    override val kodein: Kodein by Kodein.lazy {
+        import(tasksRepositoryModule)
+        bind<Context>() with provider { this@ToDoApplication }
     }
 }
